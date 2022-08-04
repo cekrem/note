3d986b6ff36c7cb8dea823a3f631dadf87d491cc
Christian Ekrem <cekrem@pm.me>
2022-08-04 09:36:36 +0200

    tweak dep installs

--- index.sh
+++ index.sh
@@ -5,37 +5,34 @@ error() {
   exit 1
 }
 
+confirm_or_fail() {
+  read -r -p "$1" answer
+  case ${answer:0:1} in
+  y | Y) ;;
+
+  *)
+    error "$2"
+    ;;
+  esac
+}
+
 require_brew() {
   command -v brew >/dev/null && return
-  read -r -p "Install homebrew (required)? (y/n)" answer
-  case ${answer:0:1} in
-  y | Y)
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    ;;
-  *)
-    error "homebrew required"
-    ;;
-  esac
+  confirm_or_fail "install homebrew? (y/n)" "can't install missing dependencies without homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || exit 1
 }
 
-require_brew_dep() {
-  command -v $dep >/dev/null && return
+require_dep() {
+  command -v "$dep" >/dev/null && return
   dep="$1"
-  read -r -p "Install missing $dep (required)? (y/n)" answer
-  case ${answer:0:1} in
-  y | Y)
-    brew install $dep
-    ;;
-  *)
-    error "$dep required"
-    ;;
-  esac
+  confirm_or_fail "$dep is not installed or not in PATH, install $dep using homebrew? (y/n)" "$dep required"
+  require_brew
+  brew install "$dep" || exit 1
 }
 
-require_brew
-brew_deps=(vim rg fzf bat)
-for dep in ${brew_deps[@]}; do
-  require_brew_dep $dep
+deps=(vim rg fzf bat)
+for dep in "${deps[@]}"; do
+  require_dep "$dep"
 done
 
 note_path=${NOTE_PATH:=$HOME/notes}
@@ -48,7 +45,8 @@ if [ $# -eq 0 ]; then
     head -n1 | awk '{print $1 " +" $2;}')
 
   if [[ "$ref" ]]; then
-    vim $ref
+    # shellcheck disable=SC2086
+    vim $ref # intentional word split: `[filename] +[line_number]`
   fi
 
   exit 0
